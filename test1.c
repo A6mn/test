1549418e7683104f6208d817f73192137aecc26c
A6mn <47579179+A6mn@users.noreply.github.com>
2019-02-15 23:20:21 +0300

    Add files via upload

---
+++ test1.c
@@ -0,0 +1,16 @@
+//ahmed muthanna
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+  printf("This program to change celsius to Kelvin\n");
+  int c,k;
+  printf("inter the voleum in celsius\n");
+  scanf("%d",&c);
+  k=c+273;
+  printf("the voleum in Kelvin = %d",k);
+
+
+}
